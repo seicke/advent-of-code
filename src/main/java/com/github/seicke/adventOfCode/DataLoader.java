db056fa7fbea47bd3d563882810c10dd2bb7754a
seicke <sebastian.eicke@gmail.com>
2025-12-30 20:12:35 +0100

    Add initial Maven configuration and Advent of Code utils

---
+++ src/main/java/com/github/seicke/adventOfCode/DataLoader.java
@@ -0,0 +1,58 @@
+package com.github.seicke.adventOfCode;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+
+public class DataLoader {
+
+	private static File resourcesDirectory = null;
+
+	static {
+
+		try {
+
+			resourcesDirectory = new File(new URI(DataLoader.class.getResource("").toString()
+					.replace(DataLoader.class.getPackageName().replace(".", "/"), "").replace("/target/classes/", "")
+					.concat("resources")).getPath());
+
+		} catch (Exception exception) {
+
+			exception.printStackTrace();
+
+		}
+
+	}
+
+	public static List<String> getStringLines(int year, int day, boolean useExampleData) {
+
+		String filename = determineDataFile(year, day, useExampleData);
+
+		File dataFile = new File(resourcesDirectory + "/" + filename);
+
+		try (FileInputStream dataFileInputStream = new FileInputStream(dataFile)) {
+
+			return IOUtils.readLines(dataFileInputStream, StandardCharsets.UTF_8);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			return new ArrayList<String>();
+
+		}
+
+	}
+
+	private static String determineDataFile(int year, int day, boolean useExampleData) {
+
+		String filename = String.format("%4d/Day%02d_%sData.txt", year, day, useExampleData ? "Example" : "");
+
+		return filename;
+	}
+
+}
---
+++ src/main/java/com/github/seicke/adventOfCode/ResultHelper.java
@@ -0,0 +1,15 @@
+package com.github.seicke.adventOfCode;
+
+public class ResultHelper {
+
+	public static String suffix(int actual, int expected) {
+
+		if (actual == expected) {
+			return "✓";
+		}
+
+		return String.format("(%s, should be %s)", (actual > expected ? "too high" : "too low"), expected);
+
+	}
+
+}
